bee8486a3f26f5474b182af617c4c38d9d08c8db
Sujith Quintelier <sujith.quintelier@euri.com>
2017-12-06 21:51:20 +0100

    Day 06 Part 1

--- AdventOfCode/Aoc2017/Day06.cs
+++ AdventOfCode/Aoc2017/Day06.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aoc2017.Helpers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2017
 {
@@ -6,7 +8,43 @@ namespace Aoc2017
     {
         public int Part1(string input)
         {
-            return int.MinValue;
+            var res = 0;
+
+            if (input.IsNullOrEmpty())
+                return int.MinValue;
+
+            //Split Into Lines
+            var numbers = input.Split('\t').Select(int.Parse).ToArray();
+
+            LinkedList<int> ll = new LinkedList<int>(numbers);
+
+            var usedCfg = new List<string> {string.Join("|", ll)};
+
+
+            while (true)
+            {
+                //Get Max Block
+                var maxBlock = ll.Max();
+
+                //Find first Node With Max Value
+                var currentNode = ll.Find(maxBlock);
+
+                // ReSharper disable once PossibleNullReferenceException
+                currentNode.Value = 0;
+
+                res++;
+                for (var i = 0; i < maxBlock; i++)
+                {
+                    currentNode = currentNode.Next ?? ll.First;
+
+                    currentNode.Value += 1;
+                }
+                if (usedCfg.Contains(string.Join("|", ll)))
+                {
+                    return res;
+                }
+                usedCfg.Add(string.Join("|", ll));
+            }
         }
 
         public int Part2(string input)

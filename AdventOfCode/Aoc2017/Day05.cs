e70d9662ffd99f4ca336194a2f30b1afd0389e90
Sujith Quintelier <sujith.quintelier@euri.com>
2017-12-06 19:33:50 +0100

    Day 05 Part 2

--- AdventOfCode/Aoc2017/Day05.cs
+++ AdventOfCode/Aoc2017/Day05.cs
@@ -61,15 +61,69 @@ namespace Aoc2017
                 }
 
             }
-
-
-
             return res;
         }
 
         public int Part2(string input)
         {
-            return int.MinValue;
+            var res = 0;
+
+            if (input.IsNullOrEmpty())
+                return int.MinValue;
+
+            //Split Into Lines
+            var numbers = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(int.Parse).ToList();
+
+            var cIdx = 0;
+
+            /*
+            (0) 3  0  1  -3  - before we have taken any steps.
+            (1) 3  0  1 - 3 - jump with offset 0(that is, don't jump at all). Fortunately, the instruction is then incremented to 1.
+           
+            2(3) 0  1 - 3 - step forward because of the instruction we just modified.The first instruction is incremented again, now to 2.
+           
+            2  4  0  1(-3) - jump all the way to the end; leave a 4 behind.
+            2(4) 0  1 - 2 - go back to where we just were; increment - 3 to - 2.
+            2  5  0  1 - 2 - jump 4 steps forward, escaping the maze.
+            */
+
+            while (true)
+            {
+                //var a = numbers.Select(s => s.ToString()).ToList();
+                //a[cIdx] = "(" + a[cIdx] + ")";
+                //Debug.Print("{0}", string.Join(" ", a));
+
+                //Determine number of moves
+                var step = numbers[cIdx];
+
+                //Increase
+                if (numbers[cIdx] > 2)
+                {
+                    numbers[cIdx] -= 1;
+                }
+                else
+                {
+                    numbers[cIdx] += 1;
+                }
+
+                //Next Index
+                cIdx += step;
+
+                try
+                {
+                    //Increase Steps
+                    res++;
+                    //Try move
+                    var tmp = numbers[cIdx];
+                }
+                catch (Exception)
+                {
+                    //Debug.Print("{0}", string.Join(" ", numbers));
+                    break;
+                }
+
+            }
+            return res;
         }
     }
 }
